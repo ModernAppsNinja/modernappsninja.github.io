c24684ecd5d2a763c584366a534f25b60fe22489
afewell <afewell@gmail.com>
2020-12-22 00:32:22 -0800

    updating orgteamregapicall.sh

--- static/admin/orgteamregapicall.sh
+++ static/admin/orgteamregapicall.sh
@@ -1,17 +1,9 @@
-# Assuming you have `export GITHUB_OAUTH_TOKEN=...` in your .bash_profile
+# Assuming you have `export GITHUB_OAUTH_TOKEN=...` in your env
 
 # Define inputs
-
-# Derive request data
-team_id="$(
-    curl -s                                                               \
-         -H "Authorization: token $GITHUB_OAUTH_TOKEN"                    \
-         "https://api.github.com/orgs/$org_login/teams"                   |\
-      jq 'map(select(.name=="Members")) | .[].id'
-)"
 json='{
   "role": "direct_member",
-  "team_ids":['$team_id'],
+  "team_ids":['4370857'],
   "invitee_id":"'$requesting_user_id'"
 }'
 
